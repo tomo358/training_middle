c5d11076ee0969e9f5aa9dfcbccc6599d47cd828
tomoki.itou <tomoki.itou@super-studio.jp>
2018-12-17 17:04:27 +0900

    Add /config/locales/ja.yml

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
 
   # GET /tasks
   # GET /tasks.json
