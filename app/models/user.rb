e710f58a2b5423f25b415b575044d907fbb26a70
tomoki.itou <tomoki.itou@super-studio.jp>
2019-01-08 14:01:03 +0900

    Implement user setting screen

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-
 end
