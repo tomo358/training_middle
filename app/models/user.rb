db03a0f7e86deda425d09e6ad2306d1e657d77ff
tomoki.itou <tomoki.itou@super-studio.jp>
2019-01-11 10:43:52 +0900

    Add mail dastination

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,7 @@
 class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
 
-  validates :name,
-    presence: true,
-    uniqueness: true
+  validates :name, presence: true
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
