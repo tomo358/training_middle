22715049fffcd7dd9d1cda0b614752e72d582043
tomoki.itou <tomoki.itou@super-studio.jp>
2019-01-17 19:38:20 +0900

    Update tasks_controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,16 +1,11 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!
   before_action :set_task, only: [:show, :edit, :update, :destroy, :assign, :assign_update]
-  before_action :set_user, only: [:mypage_show, :assign]
 
-  # GET /tasks
-  # GET /tasks.json
   def index
     @tasks = Task.all
   end
 
-  # GET /tasks/1
-  # GET /tasks/1.json
   def show
   end
 
@@ -19,94 +14,59 @@ class TasksController < ApplicationController
   end
 
   def assign
-    @user = User.all
+    @users = User.all
   end
 
   def assign_update
-    respond_to do |format|
-
-         @task = Task.find(params[:id])
-      if @task.update(user_id: user_params[:user].to_i)
-        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
-        format.json { render :show, status: :ok, location: @task }
-      else
-        format.html { render :edit }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
-      end
+    if @task.update(user_id: user_params[:user].to_i)
+      redirect_to @task, notice: 'Task was successfully updated.'
+    else
+      render :edit
     end
   end
 
-  # GET /tasks/new
   def new
     @task = Task.new
   end
 
-  # GET /tasks/1/edit
   def edit
   end
 
-  # POST /tasks
-  # POST /tasks.json
   def create
     @task = Task.new(task_params)
     @task.user_id = current_user.id
 
-    respond_to do |format|
       if @task.save
-        format.html { redirect_to @task, notice: 'Task was successfully created.' }
-        format.json { render :show, status: :created, location: @task }
+        redirect_to @task, notice: 'Task was successfully created.'
       else
-        format.html { render :new }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
+        render :new
       end
-    end
   end
 
-  # PATCH/PUT /tasks/1
-  # PATCH/PUT /tasks/1.json
   def update
-
-    respond_to do |format|
-
          @task = Task.find(params[:id])
       if @task.update(task_params)
-        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
-        format.json { render :show, status: :ok, location: @task }
+        redirect_to @task, notice: 'Task was successfully updated.'
       else
-        format.html { render :edit }
-        format.json { render json: @task.errors, status: :unprocessable_entity }
+        render :edit
       end
-    end
   end
 
-  # DELETE /tasks/1
-  # DELETE /tasks/1.json
   def destroy
     if @task.user_id == current_user.id
-      @task.destroy
-      respond_to do |format|
-        format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
-        format.json { head :no_content }
-      end
+       @task.destroy
+       redirect_to tasks_url, notice: 'Task was successfully destroyed.'
     else
-      respond_to do |format|
-        format.html { redirect_to tasks_url, notice: 'Only the person in charge can delete it.' }
-        format.json { head :no_content }
-      end
+       redirect_to tasks_url, notice: 'Only the person in charge can delete it.'
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_task
       @task = Task.find(params[:id])
     end
 
-    def set_user
-      @user = current_user.id
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
     def task_params
       params.require(:task).permit(:title, :content, :deadline, :status, :user_id_id)
     end
@@ -114,5 +74,4 @@ class TasksController < ApplicationController
     def user_params
       params.require(:task).permit(:user)
     end
-
 end
